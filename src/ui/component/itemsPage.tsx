6959aba45492cb8e382ecd1fb297edc7f3f22b18
DimaMaslo88 <dmitrij-maslo@mail.ru>
2024-03-10 19:06:30 +0300

    'fix/bags'

--- src/ui/component/itemsPage.tsx
+++ src/ui/component/itemsPage.tsx
@@ -1,6 +1,6 @@
 import React, {ChangeEvent, useState} from 'react';
 import {useSelector} from "react-redux";
-import { selectCurrenPage, selectElementsOnPage, selectItems} from "bll/selectors";
+import {selectCurrenPage, selectElementsOnPage, selectItems} from "bll/selectors";
 import {SingleItemType} from "bll/types/bll-types";
 import style from 'styles/itemsPage.module.scss'
 import {ContentPagination} from "ui/component/pagination/pagination";
@@ -9,7 +9,7 @@ import {changeFilterActions, changeFilterBrand, changeFilterTitle, changeFilterV
 import {v4 as uuidv4} from 'uuid'
 
 export const ItemsPage = React.memo(() => {
-    const dispatch = useAppDispatch()
+            const dispatch = useAppDispatch()
             const items = useSelector(selectItems)
             const currentPage = useSelector(selectCurrenPage)
             const elementOnPage = useSelector(selectElementsOnPage)
@@ -17,37 +17,37 @@ export const ItemsPage = React.memo(() => {
             const lastItemsIndex = currentPage * elementOnPage
             const firstItemsIndex = lastItemsIndex - elementOnPage
             const currentItems = items.slice(firstItemsIndex, lastItemsIndex)
-    const [title,setTitle]=useState<string>('')
-    const [value,setValue]=useState<number>(0)
-    const [brandValue,setBrandValue] =useState<string>('')
-        const buttonBrandDisabled =  brandValue.length <2
-        const buttonTitleDisabled =  title.length <2
-        const buttonPriceDisabled = value ===0
-        const onChangeHandlerTitle=(e:ChangeEvent<HTMLInputElement>)=>{
-        setTitle(e.currentTarget.value)
-    }
-    const onChangeHandlerValue=(e:ChangeEvent<HTMLInputElement>)=>{
-        setValue(+e.currentTarget.value)
-    }
-    const onChangeHandlerBrand=(e:ChangeEvent<HTMLInputElement>)=>{
-        setBrandValue(e.currentTarget.value)
-    }
+            const [title, setTitle] = useState<string>('')
+            const [value, setValue] = useState<number>(0)
+            const [brandValue, setBrandValue] = useState<string>('')
+            const buttonBrandDisabled = brandValue.length < 2
+            const buttonTitleDisabled = title.length < 2
+            const buttonPriceDisabled = value === 0
+            const onChangeHandlerTitle = (e: ChangeEvent<HTMLInputElement>) => {
+                setTitle(e.currentTarget.value)
+            }
+            const onChangeHandlerValue = (e: ChangeEvent<HTMLInputElement>) => {
+                setValue(+e.currentTarget.value)
+            }
+            const onChangeHandlerBrand = (e: ChangeEvent<HTMLInputElement>) => {
+                setBrandValue(e.currentTarget.value)
+            }
 
-    const onClickHandlerTitle=()=>{
-        dispatch(changeFilterActions('product'))
-        dispatch(changeFilterTitle(title))
-        setTitle('')
-    }
-        const onClickHandlerValue=()=>{
-            dispatch(changeFilterActions('price'))
-            dispatch(changeFilterValue(value))
-            setValue(0)
-       }
-        const onClickHandlerBrand=()=>{
-            dispatch(changeFilterActions('brand'))
-            dispatch(changeFilterBrand(brandValue))
-            setBrandValue('')
-        }
+            const onClickHandlerTitle = () => {
+                dispatch(changeFilterActions('product'))
+                dispatch(changeFilterTitle(title))
+                setTitle('')
+            }
+            const onClickHandlerValue = () => {
+                dispatch(changeFilterActions('price'))
+                dispatch(changeFilterValue(value))
+                setValue(0)
+            }
+            const onClickHandlerBrand = () => {
+                dispatch(changeFilterActions('brand'))
+                dispatch(changeFilterBrand(brandValue))
+                setBrandValue('')
+            }
 
             const rows = currentItems.map(({brand, id, product, price}: SingleItemType) => (
                 <tbody key={uuidv4()}>
@@ -63,42 +63,48 @@ export const ItemsPage = React.memo(() => {
 
                 <div className={style.searchBlock}>
                     <div className={style.inputBlock}>
-                <input placeholder='фильтр по названию' value={title} onChange={onChangeHandlerTitle} className={style.input}/>
-                <button onClick={onClickHandlerTitle}
-                        className={buttonTitleDisabled?style.buttonDisabled:style.button}
-                        disabled={buttonTitleDisabled}
-                >Поиск</button>
+                        <input placeholder='фильтр по названию' value={title} onChange={onChangeHandlerTitle}
+                               className={style.input}/>
+                        <button onClick={onClickHandlerTitle}
+                                className={buttonTitleDisabled ? style.buttonDisabled : style.button}
+                                disabled={buttonTitleDisabled}
+                        >Поиск
+                        </button>
                     </div>
                     <div className={style.inputBlock}>
-                <input  placeholder='фильтр по цене' value={value} onChange={onChangeHandlerValue}  className={style.input}/>
-                <button onClick={onClickHandlerValue}
-                        className={buttonPriceDisabled?style.buttonDisabled:style.button}
-                        disabled={buttonPriceDisabled}
-                >Поиск</button>
+                        <input placeholder='фильтр по цене' value={value} onChange={onChangeHandlerValue}
+                               className={style.input}/>
+                        <button onClick={onClickHandlerValue}
+                                className={buttonPriceDisabled ? style.buttonDisabled : style.button}
+                                disabled={buttonPriceDisabled}
+                        >Поиск
+                        </button>
                     </div>
                     <div className={style.inputBlock}>
-                <input  placeholder='фильтр по бренду' value={brandValue} onChange={onChangeHandlerBrand}  className={style.input}/>
-                <button onClick={onClickHandlerBrand}
-                        className={buttonBrandDisabled?style.buttonDisabled:style.button}
-                        disabled={buttonBrandDisabled}
-                >Поиск</button>
+                        <input placeholder='фильтр по бренду' value={brandValue} onChange={onChangeHandlerBrand}
+                               className={style.input}/>
+                        <button onClick={onClickHandlerBrand}
+                                className={buttonBrandDisabled ? style.buttonDisabled : style.button}
+                                disabled={buttonBrandDisabled}
+                        >Поиск
+                        </button>
                     </div>
                 </div>
 
                 <div className={style.tableContainer}>
-        <table className={style.table} >
-                <thead className={style.theadContainer}>
-                <tr className={style.trContainer}>
-                    <td className={style.tD}>Id</td>
-                    <td className={style.tD}>Бренд</td>
-                    <td className={style.tD}>Стоимость</td>
-                    <td className={style.tD}>Продукт</td>
-                </tr>
-                </thead>
-                {rows}
-            </table>
+                    <table className={style.table}>
+                        <thead className={style.theadContainer}>
+                        <tr className={style.trContainer}>
+                            <td className={style.tD}>Id</td>
+                            <td className={style.tD}>Бренд</td>
+                            <td className={style.tD}>Стоимость</td>
+                            <td className={style.tD}>Продукт</td>
+                        </tr>
+                        </thead>
+                        {rows}
+                    </table>
                 </div>
-        <ContentPagination elementOnPage={elementOnPage} currentPage={currentPage} totalItems={totalItems}/>
+                <ContentPagination elementOnPage={elementOnPage} currentPage={currentPage} totalItems={totalItems}/>
             </div>
         }
     )
